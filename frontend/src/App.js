44af56cdf1a1a12233911cf7c84f32a76be36697
Utkarsh Tiwari <31589202+Utkarshh0110@users.noreply.github.com>
2023-07-27 19:25:22 +0000

    frontend login and BE db connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
+import "./App.css";
+import * as React from "react";
+import { Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import ChatPage from "./Pages/ChatPage";
 
 function App() {
   return (
-    <ChakraProvider>
-      HELO
-    </ChakraProvider>
-  )
+    <div className="App">
+      <Route path="/" component={Homepage} exact />
+      <Route path="/chats" component={ChatPage} exact />
+    </div>
+  );
 }
 
 export default App;
