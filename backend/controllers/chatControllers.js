85806bf92bb4fcc75f6c4116fceb9350918b0fb5
Utkarsh Tiwari <31589202+Utkarshh0110@users.noreply.github.com>
2023-07-28 17:31:10 +0000

    login functionality and chat api routes

---
+++ backend/controllers/chatControllers.js
@@ -0,0 +1,83 @@
+const asyncHandler = require("express-async-handler");
+const Chat = require("../Models/chatModel");
+const User = require("../Models/userModel");
+
+const accessChat = asyncHandler(async (req, res) => {
+  const { userId } = req.body;
+  if (!userId) {
+    console.log("UserId param not send with request");
+    return res.sendStatus(400);
+  }
+
+  var isChat = await Chat.find({
+    isGroupChat: false,
+    $and: [
+      { users: { $elemMatch: { $eq: req.user._id } } },
+      { users: { $elemMatch: { $eq: userId } } },
+    ],
+  })
+    .populate("users", "-password")
+    .populate("latestMessage");
+
+  isChat = await User.populate(isChat, {
+    path: "latestMessage.sender",
+    select: "name pic email",
+  });
+
+  if (isChat.length > 0) {
+    res.send(isChat[0]);
+    return;
+  } else {
+    var chatData = {
+      chatName: "sender",
+      isGroupChat: false,
+      users: [req.user._id, userId],
+    };
+
+    try {
+      const createdChat = await Chat.create(chatData);
+      const fullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+        "users",
+        "-password"
+      );
+
+      res.status(200).send(fullChat);
+    } catch (err) {
+      res.status(400);
+      throw new Error(err.message);
+    }
+  }
+});
+const fetchChats = asyncHandler(async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+
+        res.status(200).send(results);
+      });
+  } catch (err) {
+    res.status(400);
+    throw new Error(err.message);
+  }
+});
+const createGroupChat = asyncHandler(async (req, res) => {});
+const renameGroup = asyncHandler(async (req, res) => {});
+const removeFromGroup = asyncHandler(async (req, res) => {});
+const addToGroup = asyncHandler(async (req, res) => {});
+
+module.exports = {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroup,
+  removeFromGroup,
+  addToGroup,
+};
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const connectDB = require("./config/db");
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
