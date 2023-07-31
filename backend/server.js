1041db64d0e12afa1eaf3d6deaed0a996119940c
Utkarsh Tiwari <31589202+Utkarshh0110@users.noreply.github.com>
2023-07-31 20:21:51 +0000

    Update .env and server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 const connectDB = require("./config/db");
 const app = express();
@@ -13,13 +14,24 @@ dotenv.config();
 connectDB();
 
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("API IS RUNNING");
-});
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// ------------------Deployment-------------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile();
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API IS RUNNING");
+  });
+}
+// ------------------Deployment-------------
 
 app.use(notFound);
 app.use(errorHandler);
