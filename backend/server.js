675e95eb15bd883e8405f99042e5e84684513e26
utkarshh0110 <utkarshh0110@gmail.com>
2023-08-01 23:07:02 +0530

    added vercel and file path modification

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ const server = app.listen(PORT, console.log("SERVER RUNNING ON PORT 5000"));
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://hhhmqy-3000.csb.app",
+    origin: "http://localhost:3000",
   },
 });
 
