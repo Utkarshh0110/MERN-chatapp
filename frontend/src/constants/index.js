d52252625e7ab66725b8a24f50dabe19ecd43038
Utkarsh Tiwari <31589202+Utkarshh0110@users.noreply.github.com>
2023-08-02 00:07:07 +0530

    Update index.js

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://localhost:5000";
-export const BASE_URL = "https://mern-chatapp-server-k9fo9whaf-utkarshtiwari996-gmailcom.vercel.app";
+export const BASE_URL = "https://mern-chatapp-server.vercel.app";
