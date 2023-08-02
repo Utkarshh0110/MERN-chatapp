021d53739b3c3c32a83364dbb6cb053ef1aecf80
Utkarsh Tiwari <31589202+Utkarshh0110@users.noreply.github.com>
2023-08-02 06:00:51 +0000

    Update db.js and index.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const MONGO_URI= "mongodb+srv://utkarshh0110:utkarshh0110@cluster0.fjsm33m.mongodb.net/";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
